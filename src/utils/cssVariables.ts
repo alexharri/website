beeab59c42c0ec663d5c30ae62852d305f33d4c9
alexharri <alexharri2919@gmail.com>
2022-11-04 09:40:50 +0000

    make scrollbar follow dark and light mode

--- src/utils/cssVariables.ts
+++ src/utils/cssVariables.ts
@@ -2,6 +2,7 @@ export const colors = {
   text: "var(--color-text)",
   blue: "var(--color-blue)",
   background: "var(--color-background)",
+  scheme: "var(--color-scheme)",
 };
 
 export const colorValues: Record<
@@ -12,11 +13,13 @@ export const colorValues: Record<
     text: "#B9DBFA",
     blue: "#399ef4",
     background: "#090D13",
+    scheme: "dark",
   },
   light: {
     text: "#141d2c",
     blue: "#399ef4",
     background: "#f9f9f9",
+    scheme: "light",
   },
 };
 
