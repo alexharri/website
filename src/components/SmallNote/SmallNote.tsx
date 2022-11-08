070626008e7116720b5a1af19f18e2cee90661ff
alexharri <alexharri2919@gmail.com>
2022-11-08 11:02:37 +0000

    make it easier to move notes closer to the elements above

--- src/components/SmallNote/SmallNote.tsx
+++ src/components/SmallNote/SmallNote.tsx
@@ -2,8 +2,13 @@ import styles from "./SmallNote.module.scss";
 
 interface Props {
   children: React.ReactNode;
+  moveCloserUpBy?: number;
 }
 
 export const SmallNote = (props: Props) => {
-  return <p className={styles.p}>Note: {props.children}</p>;
+  return (
+    <p className={styles.p} style={{ marginTop: -(props.moveCloserUpBy ?? 0) }}>
+      Note: {props.children}
+    </p>
+  );
 };
