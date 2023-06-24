1ee1fd523d940af7edd7c60ba1718e93703f9082
alexharri <alexharri2919@gmail.com>
2023-06-24 10:16:59 +0000

    fix: change clip-path to clipPath

--- src/components/Icon/SquiggleIcon10x7.tsx
+++ src/components/Icon/SquiggleIcon10x7.tsx
@@ -2,7 +2,7 @@ import { renderToStaticMarkup } from "react-dom/server";
 
 export const SquiggleIcon10x7 = (props: { color?: string }) => (
   <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#squiggle)">
+    <g clipPath="url(#squiggle)">
       <path
         d="M-3.09375 1C-0.09375 1 -1.09375 5 1.90625 5C4.90625 5 3.90625 1 6.90625 1C9.90625 1 8.90625 5 11.9062 5"
         stroke={props.color || "currentColor"}
