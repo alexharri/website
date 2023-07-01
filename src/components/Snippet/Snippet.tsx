c80fd1b608ff5c6aef390e7c336b73d8778b3a0f
alexharri <alexharri2919@gmail.com>
2023-07-01 16:58:54 +0000

    fix errors in Snippet.tsx

--- src/components/Snippet/Snippet.tsx
+++ src/components/Snippet/Snippet.tsx
@@ -1,30 +1,29 @@
 import Link from "next/link";
 import { Snippet } from "../../types/Snippet";
+import { useStyles } from "../../utils/styles";
 import { StaticCodeBlock } from "../StaticCodeBlock/StaticCodeBlock";
-import styles from "./Snippet.module.scss";
+import { SnippetStyles } from "./Snippet.styles";
 
 interface Props {
   snippet: Snippet;
 }
 
 export const SnippetLink = (props: Props) => {
+  const s = useStyles(SnippetStyles);
+
   const { snippet } = props;
 
   return (
-    <article className={styles.container}>
-      <div className={styles.linkWrapper}>
-        <Link href={`/snippets/${snippet.slug}`} className={styles.link}>
-          <h2 className={styles.title}>{snippet.title}</h2>
-          {snippet.description && <p>{snippet.description}</p>}
-        </Link>
-        {snippet.snippet && (
-          <div className={styles.codeBlockWrapper}>
-            <StaticCodeBlock language={snippet.snippet.language} small>
-              {snippet.snippet.text}
-            </StaticCodeBlock>
-          </div>
-        )}
-      </div>
+    <article>
+      <Link href={`/snippets/${snippet.slug}`} className={s("link")}>
+        <h2 className={s("title")}>{snippet.title}</h2>
+        {snippet.description && <p>{snippet.description}</p>}
+      </Link>
+      {snippet.snippet && (
+        <StaticCodeBlock language={snippet.snippet.language} small>
+          {snippet.snippet.text}
+        </StaticCodeBlock>
+      )}
     </article>
   );
 };
