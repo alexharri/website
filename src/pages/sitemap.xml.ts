181e81b4915348f580e65691d3e997df971054c4
alexharri <alexharri2919@gmail.com>
2023-07-02 12:11:52 +0000

    add sitemap.xml

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,34 @@
+import { GetServerSidePropsContext } from "next";
+import { getPosts } from "../utils/blogPageUtils";
+
+function generateSiteMap() {
+  const posts = getPosts("published");
+  const postXML = (post: typeof posts[number]) => {
+    return `
+  <url>
+    <loc>https://alexharri.com/blog/${post.slug}</loc>
+    <lastmod>${post.publishedAt}T00:00:00+00:00</lastmod>
+  </url>`.trim();
+  };
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://alexharri.com</loc>
+  </url>
+  ${posts.map(postXML).join("\n  ")}
+</urlset>
+`;
+}
+
+export async function getServerSideProps({ res }: GetServerSidePropsContext) {
+  const sitemap = generateSiteMap();
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+  return {
+    props: {},
+  };
+}
+
+export default () => {};
