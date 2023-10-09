1d89e6d005faf210d3e5461a7321833df33d2642
alexharri <alexharri2919@gmail.com>
2023-10-09 20:30:39 +0000

    update about me section

---
+++ src/components/AboutMe/AboutMe.styles.ts
@@ -0,0 +1,44 @@
+import { StyleOptions } from "../../utils/styles";
+
+export const AboutMeStyles = ({ styled, theme }: StyleOptions) => ({
+  container: styled.css`
+    display: flex;
+    gap: 32px;
+
+    @media (max-width: 500px) {
+      flex-direction: column;
+      gap: 24px;
+    }
+  `,
+
+  left: styled.css`
+    margin: 0;
+  `,
+
+  right: styled.css`
+    h2 {
+      margin: 0;
+    }
+  `,
+
+  image: styled.css`
+    width: 96px;
+    height: auto;
+    border-radius: 50%;
+  `,
+
+  links: styled.css`
+    margin-top: -8px;
+    display: flex;
+    gap: 12px;
+    align-items: center;
+
+    [data-dot] {
+      display: flex;
+      width: 5px;
+      height: 5px;
+      background: ${theme.medium500};
+      border-radius: 50%;
+    }
+  `,
+});
--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,19 +1,28 @@
+import { useStyles } from "../../utils/styles";
 import { Link } from "../Link";
+import { AboutMeStyles } from "./AboutMe.styles";
 
 export const AboutMe = () => {
+  const s = useStyles(AboutMeStyles);
+
   return (
-    <>
-      <h2>About me</h2>
-      <p>
-        My name is Alex Harri. I'm a software developer from Reykjavík, Iceland.
-      </p>
-      <p>
-        <Link href="https://github.com/alexharri">GitHub</Link>,{" "}
-        <Link href="https://alexharri.medium.com/">Medium</Link>,{" "}
-        <Link href="https://www.linkedin.com/in/alex-harri-j%C3%B3nsson-b1273613b/">
-          LinkedIn
-        </Link>
-      </p>
-    </>
+    <div className={s("container")}>
+      <div className={s("left")}>
+        <img src="/images/me.png" className={s("image")} />
+      </div>
+      <div className={s("right")}>
+        <h2>About me</h2>
+        <p>Hey, my name is Alex Harri Jónsson. Welcome to my personal website and blog!</p>
+        <p className={s("links")}>
+          <Link href="https://github.com/alexharri">GitHub</Link>
+          <span data-dot />
+          <Link href="https://www.linkedin.com/in/alex-harri-j%C3%B3nsson-b1273613b/">
+            LinkedIn
+          </Link>
+          <span data-dot />
+          <Link href="https://alexharri.medium.com/">Medium</Link>
+        </p>
+      </div>
+    </div>
   );
 };
