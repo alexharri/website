2ecf6a23fb3600834a0bc6e122968d5c879282f3
alexharri <alexharri2919@gmail.com>
2024-02-04 13:55:45 +0000

    slight tweak

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -24,8 +24,8 @@ export const AboutMe = () => {
         <p>
           I'm currently working as a Senior Software Engineer at{" "}
           <Link href="https://arkio.is">Arkio</Link>, where I'm developing Arkio's modeling core.
-          Arkio is a VR/AR-native solid modeler and model reviewer for architects supporting
-          real-time collaboration.
+          Arkio is a VR-native solid modeler and model reviewer for architects supporting real-time
+          collaboration.
         </p>
       </div>
     </section>
