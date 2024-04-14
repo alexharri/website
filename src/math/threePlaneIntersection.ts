a1104f0037ca0ee15bbe6ca9e00e77a2121944eb
alexharri <alexharri2919@gmail.com>
2024-04-14 17:53:01 +0000

    refactor

--- src/math/threePlaneIntersection.ts
+++ src/math/threePlaneIntersection.ts
@@ -3,18 +3,17 @@ import { Plane } from "./Plane";
 const EPSILON = 1e-5;
 
 export function threePlaneIntersection(p1: Plane, p2: Plane, p3: Plane) {
-  const u = p2.normal.cross(p3.normal);
-
-  const denom = p1.normal.dot(u);
+  const dir = p2.normal.cross(p3.normal);
+  const denom = p1.normal.dot(dir);
 
   if (Math.abs(denom) < EPSILON) {
     return null; // Planes do not intersect at a single point
   }
 
-  const p2n_p3d = p2.normal.multiplyScalar(p3.distance);
-  const p3n_p2d = p3.normal.multiplyScalar(p2.distance);
-  const c = p1.normal.cross(p2n_p3d.sub(p3n_p2d));
-  const d = u.multiplyScalar(p1.distance);
+  const a = p2.normal.multiplyScalar(p3.distance);
+  const b = p3.normal.multiplyScalar(p2.distance);
+  const V = p1.normal.cross(a.sub(b));
+  const U = dir.multiplyScalar(p1.distance);
 
-  return d.add(c).divideScalar(denom);
+  return U.add(V).divideScalar(denom);
 }
