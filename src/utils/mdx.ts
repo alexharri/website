92cbcdd8393890fcca3bfafebcebeb5966bec991
alexharri <alexharri2919@gmail.com>
2024-04-18 19:31:05 +0000

    remove commented out code

--- src/utils/mdx.ts
+++ src/utils/mdx.ts
@@ -1,9 +1,6 @@
 import { SerializeOptions } from "next-mdx-remote/dist/types";
-// import rehypeKatex from "rehype-katex";
-// import rehypeStringify from "rehype-stringify";
 
 export const getMdxOptions = async (): Promise<SerializeOptions["mdxOptions"]> => ({
   remarkPlugins: [(await import("remark-math")).default],
-  // rehypePlugins: [[rehypeKatex, { output: "mathml" }]],
   rehypePlugins: [[(await import("rehype-mathjax/svg")).default, { svg: { scale: 1 } }]],
 });
