164cfc05426bb996167e357f1f9bf398426c981b
alexharri <alexharri2919@gmail.com>
2024-07-07 20:23:22 +0000

    improve post watcher DX

--- lib/blog/src/watcher.ts
+++ lib/blog/src/watcher.ts
@@ -1,3 +1,4 @@
+import { currentTime } from "./time";
 import { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { useEffect, useRef, useState } from "react";
 
@@ -17,40 +18,41 @@ export function usePostWatcher(options: Options) {
   versionRef.current = version;
 
   useEffect(() => {
+    let delaySeconds = 1;
     let unmounted = false;
 
     function pollAfterDelay() {
       if (process.env.NODE_ENV === "production") return;
 
       if (unmounted) return;
-      setTimeout(poll, 1000);
+      setTimeout(poll, delaySeconds * 1000);
     }
 
     async function poll() {
       try {
-        const versionData = await fetch(`/api/blog/${slug}/version`).then(
-          (res) => res.json()
-        );
+        const versionData = await fetch(`/api/blog/${slug}/version`).then((res) => res.json());
 
         if (versionData.version === versionRef.current) {
           pollAfterDelay();
           return;
         }
 
-        const sourceData = await fetch(`/api/blog/${slug}/post`).then((res) =>
-          res.json()
-        );
+        const sourceData = await fetch(`/api/blog/${slug}/post`).then((res) => res.json());
+
+        delaySeconds = 1; // Reset delay after a successful fetch
 
         if (unmounted) return;
 
-        console.log("Fetched post");
+        console.log(`[${currentTime()}] Refreshed post content at`);
 
         setSource(sourceData.source);
         setVersion(versionData.version);
 
         pollAfterDelay();
       } catch (e) {
-        console.log(e);
+        // Progressively make the delay longer, up to a maximum of 8 seconds
+        if (delaySeconds < 8) delaySeconds *= 2;
+        console.warn(`[${currentTime()}] Failed to refresh post. Is your dev server running?`);
         pollAfterDelay();
       }
     }
