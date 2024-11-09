3856106fd7507ec6f60efe902feefcd4849ccc66
alexharri <alexharri2919@gmail.com>
2024-11-09 16:06:54 +0000

    remove old redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,7 @@ import { execSync } from "child_process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   redirects: async () => [
-    {
-      source: "/blog/bit-sets-fast-foreach",
-      destination: "/blog/bit-set-iteration",
-      permanent: true,
-    },
+    // Template: { source: "/blog/old-url", destination: "/blog/new-url", permanent: true },
   ],
 
   reactStrictMode: true,
