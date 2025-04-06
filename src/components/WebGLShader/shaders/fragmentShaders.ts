91749fc6ded9ef0350e5326dd566aa756cb3e984
alexharri <alexharri2919@gmail.com>
2025-04-06 15:24:00 +0000

    remove unused shader

--- src/components/WebGLShader/shaders/fragmentShaders.ts
+++ src/components/WebGLShader/shaders/fragmentShaders.ts
@@ -26,7 +26,6 @@ import sine_stack_3 from "./fragment/sine_stack_3";
 import sine_stack_3_LSA from "./fragment/sine_stack_3_LSA";
 import sine_stack_4 from "./fragment/sine_stack_4";
 import simplex_wave from "./fragment/simplex_wave";
-import simplex_over_time from "./fragment/simplex_over_time";
 import simplex_stack_final from "./fragment/simplex_stack_final";
 import simplex_stack_0 from "./fragment/simplex_stack_0";
 import simplex_stack_1 from "./fragment/simplex_stack_1";
@@ -79,7 +78,6 @@ export const fragmentShaderRegistry: Partial<Record<string, CreateFragmentShader
   sine_stack_3_LSA,
   sine_stack_4,
   simplex_wave,
-  simplex_over_time,
   simplex_stack_final,
   simplex_stack_0,
   simplex_stack_1,
