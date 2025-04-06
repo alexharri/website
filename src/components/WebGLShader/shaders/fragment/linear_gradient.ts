284ea1f125f046f3f4f04b916d09a5dbb69c1e23
alexharri <alexharri2919@gmail.com>
2025-04-06 13:16:30 +0000

    refactor linear_gradient shader

--- src/components/WebGLShader/shaders/fragment/linear_gradient.ts
+++ src/components/WebGLShader/shaders/fragment/linear_gradient.ts
@@ -4,14 +4,14 @@ const createFragmentShader: CreateFragmentShader = (_) => {
   return /* glsl */ `
     precision mediump float;
 
-    const float CANVAS_HEIGHT = 150.0;
+    uniform float u_h;
 
     void main() {
       vec3 color_1 = vec3(0.7, 0.1, 0.4);
       vec3 color_2 = vec3(0.9, 0.6, 0.1);
 
       float y = gl_FragCoord.y;
-      float t = y / (CANVAS_HEIGHT - 1.0);
+      float t = y / (u_h - 1.0);
 
       vec3 color = mix(color_1, color_2, t);
       gl_FragColor = vec4(color, 1.0);
