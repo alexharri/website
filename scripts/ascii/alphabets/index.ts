d969ef839a46d1b1f2431063043e3855bfe52e1d
alexharri <alexharri2919@gmail.com>
2025-11-23 14:05:59 +0000

    remove unused code

--- scripts/ascii/alphabets/index.ts
+++ scripts/ascii/alphabets/index.ts
@@ -1,43 +1,16 @@
 import { ASCII_CHARS } from "./ascii";
 import { ASCII_SHORT_CHARS } from "./ascii-short";
-import { GREEK_CHARS } from "./greek";
-import { CYRILLIC_CHARS } from "./cyrillic";
-import { SYMBOL_CHARS } from "./symbols";
-import { EXTENDED_LATIN_CHARS } from "./extended-latin";
-import { PROGRAMMING_CHARS } from "./programming";
 
 export enum Alphabet {
   ASCII = "ascii",
   ASCII_SHORT = "ascii_short",
-  GREEK = "greek",
-  CYRILLIC = "cyrillic",
-  SYMBOLS = "symbols",
-  EXTENDED_LATIN = "extended_latin",
-  PROGRAMMING = "programming",
-  ALL = "all",
 }
 
 export const ALPHABET_SETS: Record<Alphabet, string[]> = {
   [Alphabet.ASCII]: ASCII_CHARS,
   [Alphabet.ASCII_SHORT]: ASCII_SHORT_CHARS,
-  [Alphabet.GREEK]: GREEK_CHARS,
-  [Alphabet.CYRILLIC]: CYRILLIC_CHARS,
-  [Alphabet.SYMBOLS]: SYMBOL_CHARS,
-  [Alphabet.EXTENDED_LATIN]: EXTENDED_LATIN_CHARS,
-  [Alphabet.PROGRAMMING]: PROGRAMMING_CHARS,
-  [Alphabet.ALL]: [],
 };
 
 export function combineAlphabets(alphabets: Alphabet[]): string[] {
-  // Handle "all" shortcut
-  if (alphabets.includes(Alphabet.ALL)) {
-    const allAlphabets = Object.values(Alphabet).filter(
-      (a) => a !== Alphabet.ALL
-    ) as Alphabet[];
-    return [
-      ...new Set(allAlphabets.flatMap((alphabet) => ALPHABET_SETS[alphabet])),
-    ];
-  }
-
   return [...new Set(alphabets.flatMap((alphabet) => ALPHABET_SETS[alphabet]))];
 }
