11d82cdd40ed61a8ebf20d3fe8f50406c4a5aa98
alexharri <alexharri2919@gmail.com>
2026-01-14 14:50:47 +0000

    remove duplicate EFFECTS

--- src/components/AsciiScene/render/AsciiRenderer.tsx
+++ src/components/AsciiScene/render/AsciiRenderer.tsx
@@ -11,27 +11,7 @@ import { AsciiRenderConfig } from "../renderConfig";
 import { DebugVizOptions } from "../types";
 import { hexToRgbaString } from "../../../utils/color";
 import { Observer } from "../../../utils/observer";
-
-export const EFFECTS = {
-  componentWiseGlobalNormalization(vectors: number[][]) {
-    const K = vectors[0].length;
-    const maxValues: number[] = vectors[0].map(() => 0);
-
-    for (const vector of vectors) {
-      for (let i = 0; i < K; i++) {
-        maxValues[i] = Math.max(maxValues[i], vector[i]);
-      }
-    }
-
-    const scalars = maxValues.map((v) => 1 / v);
-
-    for (const vector of vectors) {
-      for (let i = 0; i < K; i++) {
-        vector[i] = vector[i] * scalars[i];
-      }
-    }
-  },
-};
+import { EFFECTS } from "../effects";
 
 interface Props {
   samplingDataObserver: Observer<CharacterSamplingData[][]>;
